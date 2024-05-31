bfb1d25b3cbe88e1d3bb22c2e5adfc32e962f8ce
Christian Estupinan <100639879+Ivesqui@users.noreply.github.com>
2024-05-30 22:01:10 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+
+use thiagoalessio\TesseractOCR\TesseractOCR;
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+set_time_limit(0);
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method != 'POST') {
+    exit;
+}
+
+$basePath = dirname(__FILE__);
+
+require_once "{$basePath}/vendor/autoload.php";
+
+function downloadFile($url, $path)
+{
+    $newfname = $path;
+    $file = fopen ($url, 'rb');
+    if ($file) {
+        $newf = fopen ($newfname, 'wb');
+        if ($newf) {
+            while(!feof($file)) {
+                fwrite($newf, fread($file, 1024 * 8), 1024 * 8);
+            }
+        }
+    }
+    if ($file) {
+        fclose($file);
+    }
+    if ($newf) {
+        fclose($newf);
+    }
+}
+
+$fileUrl = $_POST['file_url'];
+$pdfPath = "{$basePath}/temp/temp.pdf";
+$imagePath = "{$basePath}/temp/temp.png";
+
+
+downloadFile($fileUrl, $pdfPath);
+
+$pdf = new \Spatie\PdfToImage\Pdf($pdfPath);
+$pdf->saveImage($imagePath);
+
+$ocr = (new TesseractOCR($imagePath))->lang('deu', 'eng');
+
+try {
+    $content = $ocr->run();
+    $success = true;
+} catch (\Throwable $e) {
+    $success = false;
+    $content = $e->getMessage();
+} finally {
+    unlink($pdfPath);
+    unlink($imagePath);
+
+    header('HTTP/1.1 200 OK');
+
+    echo json_encode([
+        'success' => $success,
+        'content' => trim($content)
+    ]);
+}
+
+exit;
